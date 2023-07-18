27d22f23ccb3d0e8c724a496f4e7e2535d5a4ff3
HardRockLogic <neiton5001@gmail.com>
2023-07-18 16:29:39 +0300

    d16 parsing done

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,17 @@
 mod parse;
 
+use nom::Finish;
+use std::{fs, println};
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("test.txt").unwrap();
+
+    let valves: Vec<_> = file
+        .lines()
+        .map(|line| parse::Valve::scrap_valve(line).finish().unwrap().1)
+        .collect();
+
+    for valve in &valves {
+        println!("{valve:?}");
+    }
 }
--- day16/src/parse.rs
+++ day16/src/parse.rs
@@ -2,12 +2,67 @@
 
 pub struct Name([u8; 2]);
 
+impl fmt::Debug for Name {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let [a, b] = self.0;
+        write!(f, "{}{}", a as char, b as char)
+    }
+}
+impl fmt::Display for Name {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+impl Name {
+    fn scrap_name(i: &str) -> IResult<&str, Self> {
+        map(take(2usize), |slice: &str| {
+            Self(slice.as_bytes().try_into().unwrap())
+        })(i)
+    }
+}
+
+#[derive(Debug)]
 pub struct Valve {
     name: Name,
     flow: u32,
     adjecents: Vec<Name>,
 }
 
+use core::fmt;
+use std::write;
+
 use nom::{
-    bytes::complete::tag, combinator::map, multi::separated_list1, sequence::tuple, IResult,
+    branch::alt,
+    bytes::complete::{tag, take},
+    character::complete as cc,
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, tuple},
+    IResult,
 };
+
+impl Valve {
+    pub fn scrap_valve(i: &str) -> IResult<&str, Self> {
+        let (i, (name, flow, adjecents)) = tuple((
+            preceded(tag("Valve "), Name::scrap_name),
+            preceded(tag(" has flow rate="), cc::u32),
+            preceded(
+                alt((
+                    tag("; tunnel leads to valve "),
+                    tag("; tunnels lead to valves "),
+                )),
+                separated_list1(tag(", "), Name::scrap_name),
+            ),
+        ))(i)?;
+
+        Ok((
+            i,
+            Self {
+                name,
+                flow,
+                adjecents,
+            },
+        ))
+    }
+}
