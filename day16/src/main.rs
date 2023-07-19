6ae15a7006eff3f7ed5e2e09868cf7e06dceb700
HardRockLogic <neiton5001@gmail.com>
2023-07-20 01:09:55 +0300

    d16 working greedy search

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -7,13 +7,15 @@ use parse::{Name, Valve};
 use std::{
     collections::{HashMap, VecDeque},
     fs,
+    iter::once,
 };
 
 #[derive(Debug)]
-struct ProtoState {
+struct State {
     lable: Name,
     value: u32,
     time: u32,
+    history: Vec<Name>,
 }
 
 #[derive(Debug, Default)]
@@ -36,33 +38,41 @@ impl DistMap {
             dist.data
                 .insert(valve.name, (i, valve.flow, valve.adjecents));
         }
-
         dist
     }
-
-    fn best_option_greedy(&self, valve: Name, time: u32) -> Vec<ProtoState> {
+    // struct State {
+    //     lable: Name,
+    //     value: u32,
+    //     time: u32,
+    //     history: Vec<Name>,
+    // }
+    fn best_option_greedy(&self, state: State) -> Vec<State> {
         let mut queue: VecDeque<(Name, u32)> = VecDeque::new();
         let mut enqueued: Vec<Name> = Vec::new();
-        let mut options: Vec<ProtoState> = Vec::new();
-        queue.push_back((valve, time));
+        let mut options: Vec<State> = Vec::new();
+        queue.push_back((state.lable, state.time));
 
         while !queue.is_empty() {
             let (name, elapsed) = queue.pop_front().unwrap();
+
             if elapsed <= 1 {
                 break;
             }
+
             let node = self.data.get(&name).unwrap();
-            if node.1 != 0 {
-                options.push(ProtoState {
+
+            if node.1 != 0 && !state.history.contains(&name) {
+                options.push(State {
                     lable: name,
-                    value: node.1 * (elapsed - 1),
+                    value: state.value + node.1 * (elapsed - 1),
                     time: elapsed - 1,
+                    history: state.history.iter().copied().chain(once(name)).collect(),
                 });
             }
-            node.2.iter().copied().for_each(|v| {
+            node.2.iter().for_each(|v| {
                 if !enqueued.contains(&v) {
-                    queue.push_back((v, elapsed - 1));
-                    enqueued.push(v);
+                    queue.push_back((*v, elapsed - 1));
+                    enqueued.push(*v);
                 }
             });
         }
@@ -70,27 +80,40 @@ impl DistMap {
     }
 }
 
-#[derive(Debug)]
-struct Valves {
-    map: Vec<Valve>,
-}
-
-impl Valves {
-    fn init(s: &str) -> Self {
-        Self {
-            map: s
-                .lines()
-                .map(|line| Valve::scrap_valve(line).finish().unwrap().1)
-                .collect(),
-        }
-    }
-}
+// #[derive(Debug)]
+// struct Valves {
+//     map: Vec<Valve>,
+// }
+//
+// impl Valves {
+//     fn init(s: &str) -> Self {
+//         Self {
+//             map: s
+//                 .lines()
+//                 .map(|line| Valve::scrap_valve(line).finish().unwrap().1)
+//                 .collect(),
+//         }
+//     }
+// }
 
 fn main() {
     let file = fs::read_to_string("test.txt").unwrap();
 
     let map = DistMap::init(&file);
-    dbg!(map.best_option_greedy(Name::from("AA"), 30));
+    let state = State {
+        lable: Name::from("EE"),
+        value: 1639,
+        time: 9,
+        history: vec![
+            Name::from("DD"),
+            Name::from("BB"),
+            Name::from("JJ"),
+            Name::from("HH"),
+            Name::from("EE"),
+        ],
+    };
+
+    dbg!(map.best_option_greedy(state));
 
     // for valve in &map.map {
     //     println!("{valve:?}");
